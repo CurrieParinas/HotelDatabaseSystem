246f3ad158af6bb161b4aa5854715ce1b5e91312
Demi Gail Ashley Ercia <deercia@up.edu.ph>
2024-01-13 13:05:37 +0800

    menu and services bare bones

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import './Services.css'
 
 function Services() {
   return (
-    <div>Services</div>
+    <section className="services">
+        <div className="servicesContainer">
+            <div className="servicesHeader">
+            <h2 className=  "servicessecTitle">Services</h2>
+            </div>
+
+
+
+            <div className="servicesmainContent servicesContent grid">
+                {
+                  <div className="servicesParentDiv">
+                <div className="servicessingleRoom">
+                  <div className="servicesImage">
+                  <h3> <center>Filler</center></h3>
+                        {/* <img src={imgSrc} alt="" /> */}
+                  </div>
+                  <div className="servicesFooter">
+                      <div className="servicesText">
+                          <h6>Spa1</h6>
+                      
+                      </div>
+                  </div>
+                  
+                </div>
+               
+                <div className="servicessingleRoom">
+                  <div className="servicesImage">
+                  <h3> <center>Filler</center></h3>
+                        {/* <img src={imgSrc} alt="" /> */}
+                  </div>
+                  <div className="servicesFooter">
+                      <div className="servicesText">
+                          <h6>Spa2</h6>
+                      
+                      </div>
+                  </div>
+                  
+                </div><div className="servicessingleRoom">
+                  <div className="servicesImage">
+                  <h3> <center>Filler</center></h3>
+                        {/* <img src={imgSrc} alt="" /> */}
+                  </div>
+                  <div className="servicesFooter">
+                      <div className="servicesText">
+                          <h6>Spa3</h6>
+                      
+                      </div>
+                  </div>
+                  
+                </div>
+                
+                
+                
+                </div> //parent div
+                
+                }
+            </div>
+        </div>
+    </section>
   )
 }
+  
+ 
 
-export default Services
+
+export default Services;
