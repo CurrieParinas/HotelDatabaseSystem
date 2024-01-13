d28145163cb4c1a2f3be15b427f38e164ae880a2
msrosales <msrosales@up.edu.ph>
2024-01-13 13:25:26 +0800

    updated login

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -1,14 +1,11 @@
 import React,{useState,useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import './LoginPage.css';
-//import IMAGES from './Components/Photos/Photos.js';
 
 const LoginPage = () =>{
-    const [employee,setEmployee] = useState({
-        email:'',
-        password:''
-    });
     const [inputEmail, setInputEmail] = useState('');
     const [inputPassword,setInputPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         const target = e.target;
@@ -29,21 +26,23 @@ const LoginPage = () =>{
             email:inputEmail,
             password: inputPassword
         };
-        
-
-        setEmployee(inputEmployee);
+    
         console.log(inputEmployee);
 
         try{
-            const response = await fetch(`http://localhost:8080/miancurocho/accounts/getId?accountEmail=${inputEmployee.email}&accountPassword=${inputEmployee.password}`)
+            const response = await fetch(`http://localhost:8080/miancurocho/accounts/getIdAndType?email=${inputEmployee.email}&password=${inputEmployee.password}`)
             if(response.ok){
-                const data = await response.text();
+                const data = await response.json();
                 //PERFORM NAVIGATION HERE
-                if(data === ''){
+                if(data.length === 0){
                     console.log('Invalid password')
                 }
                 else{
-                    console.log(data)
+                    
+                    
+                navigate(`/${data[0].EMPLOYEE_TYPE}/dashboard/${data[0].EMPLOYEE_ID}`)
+                        
+                    
                 }
             }else {
                 console.log('Server returned an error:', response.statusText);
@@ -51,39 +50,37 @@ const LoginPage = () =>{
 
         }   catch (error){
             console.log('Error occured while logging in',error)
-        }
-        
+        }    
     }
 
     return(
         <section className = "login_container">
-            <div className = "login_body">
-                <h1 className = "login_header">Login</h1>
-                <div className = "login_components">
-                    <h1 className = "login_subtitle">Email</h1>
-                    <input className = "login_input" name="inputEmail" value={inputEmail} onChange={handleChange} />
-                    <h1 className = "login_subtitle">Password</h1>
-                    <input className = "login_input" name="inputPassword" value={inputPassword} onChange={handleChange} />
-                    
-                    <div className = "login_button_comp ">
-                        <button className = "login_button btn" onClick={handleSubmitLogin}>LOGIN</button>
-                    </div>
-                    
-                </div> 
-            </div>
-
-            {/* <div className = "login_body">
-
-            <div className = "login_photos">
-                <div>
-                    <img src={IMAGES.imgBar1} alt ="" />
+        <div className = "login_body">
+            <h1 className = "login_header">Login</h1>
+            <div className = "login_components">
+                <h1 className = "login_subtitle">Email</h1>
+                <input className = "login_input" name="inputEmail" value={inputEmail} onChange={handleChange} />
+                <h1 className = "login_subtitle">Password</h1>
+                <input className = "login_input" name="inputPassword" value={inputPassword} onChange={handleChange} type="password" />
+                
+                <div className = "login_button_comp ">
+                    <button className = "login_button btn" onClick={handleSubmitLogin}>LOGIN</button>
                 </div>
-            </div>
+                
+            </div> 
+        </div>
 
-            </div> */}
-        </section> 
+        {/* <div className = "login_body">
+
+        <div className = "login_photos">
+            <div>
+                <img src={IMAGES.imgBar1} alt ="" />
+            </div>
+        </div>
+
+        </div> */}
+    </section> 
     )
 }
 
 export default LoginPage;
-
